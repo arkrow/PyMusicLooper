a54a0a4741704511703d09094ac22735e6e6aeb8
Hazem Nabil <arkrow@protonmail.com>
2020-04-01 15:34:03 +0200

    Added angle method; sim/dur priority switch

--- looper.py
+++ looper.py
@@ -25,7 +25,7 @@ class MusicLooper:
         self.channels = self.playback_audio.shape[0]
         self.encoding = mpg123.ENC_FLOAT_32
 
-    def find_loop_pairs(self, min_duration_multiplier=0.2):
+    def find_loop_pairs(self, method='angle', min_duration_multiplier=0.2):
         _, beats = librosa.beat.beat_track(y=self.audio, sr=self.rate)
         chroma = librosa.feature.chroma_stft(y=self.audio, sr=self.rate)
         mel_spectrogram = librosa.feature.melspectrogram(y=self.audio, sr=self.rate)
@@ -38,23 +38,43 @@ class MusicLooper:
                 if beats[i] - beats[j] < min_duration:
                     continue
                 
-                dist = np.linalg.norm(chroma[..., beats[i]] - chroma[..., beats[j]])
-
-                if dist <= 0.1:
-                    candidate_pairs.append((beats[j], beats[i], dist))
+                if method == 'euclid_dist':
+                    dist = np.linalg.norm(chroma[..., beats[i]] - chroma[..., beats[j]])
+                    if dist <= 0.15:
+                        candidate_pairs.append((beats[j], beats[i], dist))
         
+                elif method == 'angle':
+                    angle = np.abs(self.angle_between(chroma[..., beats[i]], chroma[..., beats[j]]))           
+                    if angle <= 10:
+                        candidate_pairs.append((beats[j], beats[i], angle))
+        
+        print(len(candidate_pairs))
         most_similar_pairs = sorted(candidate_pairs, reverse=False, key=lambda x: x[2])[:10]
         pruned_list = []
+
+        print(most_similar_pairs)
 
         for start, end, dist in most_similar_pairs:
             if self._is_db_similar(power_db[..., end], power_db[..., start], threshold=2.5):
                 pruned_list.append((start, end, dist))
 
+        print(pruned_list)
+
         return pruned_list
 
     def _is_db_similar(self, power_db_f1, power_db_f2, threshold):
         return np.abs(np.average(power_db_f1) - np.average(power_db_f2)) <= threshold
+    
+    def unit_vector(self, vector):
+        """ Returns the unit vector of the vector """
+        return vector / np.linalg.norm(vector)
 
+    def angle_between(self, v1, v2):
+        """ Returns the angle in degrees between vectors 'v1' and 'v2' """
+        v1_u = self.unit_vector(v1)
+        v2_u = self.unit_vector(v2)
+        return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)) * 360 / np.pi
+    
     def frames_to_samples(self, frame):
         return librosa.core.frames_to_samples(frame)
 
@@ -73,7 +93,7 @@ class MusicLooper:
         adjusted_start_offset = start_offset * self.channels
         adjusted_loop_offset = loop_offset * self.channels
 
-        i = 0
+        i = adjusted_loop_offset - 1000
         try:
             while True:
                 out.play(playback_frames[..., i])
@@ -85,7 +105,7 @@ class MusicLooper:
         except KeyboardInterrupt:
             print() # so that the program ends on a newline
 
-def loop_track(filename):
+def loop_track(filename, prioritize_duration=False):
     try:
         # Load the file
         print("Loading {}...".format(filename))
@@ -93,12 +113,21 @@ def loop_track(filename):
 
         a = track.find_loop_pairs()
         # Use the loop point with the best similarity
-        start, end, dist = a[0]
+        if len(a) == 0:
+            print('No suitable loop point found.')
 
-        print("Playing with loop from {} back to {} (dist={})".format(
+        if prioritize_duration:
+            a = sorted(a, key=lambda x: np.abs(x[0] - x[1]), reverse=True)
+        else:
+            a = sorted(a, key=lambda x: x[-1])
+
+        start, end, score = a[0]
+        
+        print("Playing with loop from {} back to {}, prioritizing {}, (score={})".format(
             track.frames_to_ftime(end),
             track.frames_to_ftime(start),
-            dist))
+            'duration' if prioritize_duration else 'beat similarity',
+            score))
         print("(press Ctrl+C to exit)")
         track.play_looping(start, end)
 
