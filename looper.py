84b5c63877e995a1ba524991fa05bb3fd465f050
Hazem Nabil <arkrow@protonmail.com>
2020-04-02 18:41:44 +0200

    Added correlation method for loop finding; added plp as beat finding alternative; refactored code for added parameters; added lag finder for sample alignment

--- looper.py
+++ looper.py
@@ -10,26 +10,37 @@ class MusicLooper:
         # Load the file if it exists
         if os.path.exists(filename) and os.path.isfile(filename):
             try:
-                audio, sr = librosa.load(filename, sr=None, mono=True)
+                audio, sr = librosa.load(filename, sr=None, mono=False)
             except:
                 raise TypeError("Unsupported file type.")
         else:
             raise FileNotFoundError("Specified file not found.")
 
         # Get the waveform data from the mp3 file
-        self.audio = audio
+        self.audio = librosa.core.to_mono(audio)
         self.rate = sr
-        self.playback_audio, _ = librosa.load(filename, sr=None, mono=False)
+        self.playback_audio = audio
 
         # Initialize parameters for playback
         self.channels = self.playback_audio.shape[0]
         self.encoding = mpg123.ENC_FLOAT_32
 
-    def find_loop_pairs(self, method='angle', min_duration_multiplier=0.2):
-        _, beats = librosa.beat.beat_track(y=self.audio, sr=self.rate)
+    def find_loop_pairs(self, method='corr', min_duration_multiplier=0.2, use_plp=False, keep_at_most=10):
+        
+        if use_plp:
+            # onset_env = librosa.onset.onset_strength(y=self.audio, sr=self.rate)
+            n = (self.rate / 22050) * 384
+            pulse = librosa.beat.plp(y=self.audio, sr=self.rate, win_length=n)
+            beats = np.flatnonzero(librosa.util.localmax(pulse))
+        else:
+            _, beats = librosa.beat.beat_track(y=self.audio, sr=self.rate)
+
         chroma = librosa.feature.chroma_stft(y=self.audio, sr=self.rate)
-        mel_spectrogram = librosa.feature.melspectrogram(y=self.audio, sr=self.rate)
-        power_db = librosa.power_to_db(mel_spectrogram)
+        S = librosa.core.stft(y=self.audio)
+        S_weighed = librosa.core.perceptual_weighting(S, librosa.fft_frequencies(sr=self.rate))
+        mel_spectrogram = librosa.feature.melspectrogram(S=S_weighed)
+
+        power_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
         min_duration = int(chroma.shape[-1] * min_duration_multiplier)
         candidate_pairs = []
 
@@ -47,16 +58,22 @@ class MusicLooper:
                     angle = np.abs(self.angle_between(chroma[..., beats[i]], chroma[..., beats[j]]))           
                     if angle <= 10:
                         candidate_pairs.append((beats[j], beats[i], angle))
+                elif method == 'corr':
+                    corr = np.corrcoef(chroma[..., beats[i]], chroma[..., beats[j]])
+                    corr = np.min(corr.flatten())
+                    if corr >= 0.99:
+                        candidate_pairs.append((beats[j], beats[i], corr))
         
         print(len(candidate_pairs))
-        most_similar_pairs = sorted(candidate_pairs, reverse=False, key=lambda x: x[2])[:10]
-        pruned_list = []
+        most_similar_pairs = []
 
-        print(most_similar_pairs)
-
-        for start, end, dist in most_similar_pairs:
-            if self._is_db_similar(power_db[..., end], power_db[..., start], threshold=2.5):
-                pruned_list.append((start, end, dist))
+        for start, end, dist in candidate_pairs:
+            if self._is_db_similar(power_db[..., end], power_db[..., start], threshold=2):
+                most_similar_pairs.append((start, end, dist))
+        
+        use_decending = True if method == 'corr' else False
+        
+        pruned_list = sorted(most_similar_pairs, reverse=use_decending, key=lambda x: x[2])[:keep_at_most]
 
         print(pruned_list)
 
@@ -105,31 +122,65 @@ class MusicLooper:
         except KeyboardInterrupt:
             print() # so that the program ends on a newline
 
-def loop_track(filename, prioritize_duration=False):
+def lag_finder(y1, y2):
+    diff = np.empty(y2.size)
+    for i in range(y2.size):
+        diff[i] = np.abs(y1[..., 0]-y2[..., i])
+    arg_min = np.argmin(diff) 
+    print(arg_min)
+    print(diff[arg_min])
+    return np.argmin(diff)
+    # from scipy import signal, fftpack
+    # import matplotlib.pyplot as plt
+    # n = len(y1)
+    # corr = np.correlate(y2, y1, mode='same') #/ np.sqrt(np.correlate(y1, y1, mode='same')[int(n/2)] * np.correlate(y2, y2, mode='same')[int(n/2)])
+    # print('y2 is ' + str(np.argmax(corr)) + 'samples behind y1')
+    # plt.figure()
+    # plt.plot(delay_arr, corr)
+    # plt.title('Lag: ' + str(np.round(delay, 3)) + ' s')
+    # plt.xlabel('Lag')
+    # plt.ylabel('Correlation coeff')
+    # plt.show()
+
+    # A = fftpack.fft(y1)
+    # B = fftpack.fft(y2)
+    # Ar = -A.conjugate()
+    # Br = -B.conjugate()
+    # # print(np.argmax(signal.correlate(a,b)))
+    # # import pdb; pdb.set_trace()
+    # # print(np.argmin(np.abs(np.subtract(a, b))))
+    # print(np.argmax(np.abs(fftpack.ifft(A*Br))))
+
+def loop_track(filename, prioritize_duration=False, start_offset=None, loop_offset=None):
     try:
         # Load the file
         print("Loading {}...".format(filename))
         track = MusicLooper(filename)
+        if start_offset is None and loop_offset is None:
+            a = track.find_loop_pairs()
+            # Use the loop point with the best similarity
+            if len(a) == 0:
+                print('No suitable loop point found.')
+                exit()
 
-        a = track.find_loop_pairs()
-        # Use the loop point with the best similarity
-        if len(a) == 0:
-            print('No suitable loop point found.')
+            if prioritize_duration:
+                a = sorted(a, key=lambda x: np.abs(x[0] - x[1]), reverse=True)
 
-        if prioritize_duration:
-            a = sorted(a, key=lambda x: np.abs(x[0] - x[1]), reverse=True)
+            start_offset, loop_offset, score = a[0]
         else:
-            a = sorted(a, key=lambda x: x[-1])
-
-        start, end, score = a[0]
+            score = None
+        start_s = track.frames_to_samples(start_offset)
+        end_s = track.frames_to_samples(loop_offset)
+        # lag_finder(track.playback_audio[0, start_s:start_s+512], track.playback_audio[0, end_s:end_s+512])
         
         print("Playing with loop from {} back to {}, prioritizing {}, (score={})".format(
-            track.frames_to_ftime(end),
-            track.frames_to_ftime(start),
+            track.frames_to_ftime(loop_offset),
+            track.frames_to_ftime(start_offset),
             'duration' if prioritize_duration else 'beat similarity',
-            score))
+            score if score is not None else 'unknown'))
         print("(press Ctrl+C to exit)")
-        track.play_looping(start, end)
+        # print(track.frames_to_samples(loop_offset), '\n', track.frames_to_samples(start_offset))
+        track.play_looping(start_offset, loop_offset)
 
     except (TypeError, FileNotFoundError) as e:
         print("Error: {}".format(e))
