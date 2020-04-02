33e70f18fabecbeaef16832ceb658ef1ed0bd679
Hazem Nabil <arkrow@protonmail.com>
2020-04-03 00:34:27 +0200

    Added augmented correlation-based comparison (corr_mod)

--- looper.py
+++ looper.py
@@ -25,7 +25,7 @@ class MusicLooper:
         self.channels = self.playback_audio.shape[0]
         self.encoding = mpg123.ENC_FLOAT_32
 
-    def find_loop_pairs(self, method='corr', min_duration_multiplier=0.2, use_plp=False, keep_at_most=10):
+    def find_loop_pairs(self, method='corr_mod', min_duration_multiplier=0.5, use_plp=False, keep_at_most=4):
         
         if use_plp:
             # onset_env = librosa.onset.onset_strength(y=self.audio, sr=self.rate)
@@ -35,10 +35,12 @@ class MusicLooper:
         else:
             _, beats = librosa.beat.beat_track(y=self.audio, sr=self.rate)
 
-        chroma = librosa.feature.chroma_stft(y=self.audio, sr=self.rate)
         S = librosa.core.stft(y=self.audio)
-        S_weighed = librosa.core.perceptual_weighting(S, librosa.fft_frequencies(sr=self.rate))
+        S_power = np.abs(S)**2
+        S_weighed = librosa.core.perceptual_weighting(S_power, librosa.fft_frequencies(sr=self.rate))
         mel_spectrogram = librosa.feature.melspectrogram(S=S_weighed)
+
+        chroma = librosa.feature.chroma_stft(S=S_power, sr=self.rate)
 
         power_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
         min_duration = int(chroma.shape[-1] * min_duration_multiplier)
@@ -55,15 +57,22 @@ class MusicLooper:
                         candidate_pairs.append((beats[j], beats[i], dist))
         
                 elif method == 'angle':
-                    angle = np.abs(self.angle_between(chroma[..., beats[i]], chroma[..., beats[j]]))           
+                    angle = np.abs(self.angle_between(chroma[..., beats[i]], chroma[..., beats[j]]))
                     if angle <= 10:
                         candidate_pairs.append((beats[j], beats[i], angle))
+                
                 elif method == 'corr':
                     corr = np.corrcoef(chroma[..., beats[i]], chroma[..., beats[j]])
                     corr = np.min(corr.flatten())
                     if corr >= 0.99:
                         candidate_pairs.append((beats[j], beats[i], corr))
-        
+
+                elif method == 'corr_mod':
+                    corr = np.corrcoef(chroma[..., beats[i]], chroma[..., beats[j]])
+                    corr = np.min(corr.flatten())
+                    if corr >= 0.99 and np.linalg.norm(chroma[..., beats[i]] - chroma[..., beats[j]]) <= np.min([np.linalg.norm(chroma[..., beats[j]] - (chroma[..., beats[j]] * 1.1)), np.linalg.norm(chroma[..., beats[i]] - (chroma[..., beats[i]] * 1.1))]):# and np.abs(self.angle_between(chroma[..., beats[i]], chroma[..., beats[j]])) <= 10:
+                        candidate_pairs.append((beats[j], beats[i], corr))
+
         print(len(candidate_pairs))
         most_similar_pairs = []
 
@@ -71,7 +80,7 @@ class MusicLooper:
             if self._is_db_similar(power_db[..., end], power_db[..., start], threshold=2):
                 most_similar_pairs.append((start, end, dist))
         
-        use_decending = True if method == 'corr' else False
+        use_decending = True if method == 'corr' or method == 'corr_mod' else False
         
         pruned_list = sorted(most_similar_pairs, reverse=use_decending, key=lambda x: x[2])[:keep_at_most]
 
@@ -96,7 +105,7 @@ class MusicLooper:
         return librosa.core.frames_to_samples(frame)
 
     def frames_to_ftime(self, frame):
-        time_sec = librosa.core.frames_to_time(frame, sr=self.rate)
+        time_sec = librosa.core.frames_to_time(frame, sr=self.rate, n_fft=2048)
         return "{:02.0f}:{:06.3f}".format(
                     time_sec // 60,
                     time_sec % 60
@@ -110,7 +119,7 @@ class MusicLooper:
         adjusted_start_offset = start_offset * self.channels
         adjusted_loop_offset = loop_offset * self.channels
 
-        i = adjusted_loop_offset - 1000
+        i = adjusted_loop_offset - 1500
         try:
             while True:
                 out.play(playback_frames[..., i])
