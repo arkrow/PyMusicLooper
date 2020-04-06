29bcf75b5cfd9bc0a1a8ef519a7c6dd8c9df0051
Hazem Nabil <arkrow@protonmail.com>
2020-04-06 14:48:42 +0200

    Refactored code to avoid duplicating work when rerunning with different params; keep_at_most is now max(25% of results_len, 10)

--- looper.py
+++ looper.py
@@ -29,7 +29,7 @@ class MusicLooper:
         self.channels = self.playback_audio.shape[0]
         self.encoding = mpg123.ENC_FLOAT_32
 
-    def _loop_finding_routine(self, beats, i_start, i_stop, chroma, power_db, min_duration):
+    def _loop_finding_routine(self, beats, i_start, i_stop, chroma, power_db, min_duration, avg_dB_diff_threshold):
         for i in range(i_start, i_stop):
             deviation = np.linalg.norm(chroma[..., beats[i]] * 0.1)
             for j in range(i):
@@ -39,30 +39,22 @@ class MusicLooper:
                 dist = np.linalg.norm(chroma[..., beats[i]] - chroma[..., beats[j]])
                 if dist <= deviation:
                     avg_db_diff = self.db_diff(power_db[..., beats[i]], power_db[..., beats[j]])
-                    if avg_db_diff <= 10:
+                    if avg_db_diff <= avg_dB_diff_threshold:
                         self._candidate_pairs_q.put((beats[j], beats[i], avg_db_diff))
 
     def db_diff(self, power_db_f1, power_db_f2):
         average_diff = np.average(np.abs(power_db_f1 - power_db_f2))
         return average_diff
 
-    def find_loop_pairs(self, min_duration_multiplier=0.35, combine_beat_plp=False, keep_at_most=16, concurrency=True):
+    def find_loop_pairs(self, min_duration_multiplier=0.35, combine_beat_plp=False, concurrency=True):
         runtime_start = time.time()
 
         S = librosa.core.stft(y=self.audio)
         S_power = np.abs(S)**2
         S_weighed = librosa.core.perceptual_weighting(S_power, librosa.fft_frequencies(sr=self.rate))
         mel_spectrogram = librosa.feature.melspectrogram(S=S_weighed)
-
-        if combine_beat_plp:
-            onset_env = librosa.onset.onset_strength(S=mel_spectrogram)
-            pulse = librosa.beat.plp(onset_envelope=onset_env)
-            beats_plp = np.flatnonzero(librosa.util.localmax(pulse))
-            bpm, beats_bt = librosa.beat.beat_track(onset_envelope=onset_env)
-            beats = np.union1d(beats_bt, beats_plp)
-        else:
-            onset_env = librosa.onset.onset_strength(S=mel_spectrogram)
-            bpm, beats = librosa.beat.beat_track(onset_envelope=onset_env)
+        onset_env = librosa.onset.onset_strength(S=mel_spectrogram)
+        bpm, beats = librosa.beat.beat_track(onset_envelope=onset_env)
 
         beats = np.sort(beats)
         print('Detected {} beats at {} bpm'.format(beats.size, bpm))
@@ -71,48 +63,65 @@ class MusicLooper:
 
         power_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
         min_duration = int(chroma.shape[-1] * min_duration_multiplier)
-        candidate_pairs = []
 
         self._candidate_pairs_q = Manager().Queue()
 
         runtime_end = time.time()
         print('Finished prep in {}s'.format(runtime_end - runtime_start))
 
-        if concurrency:
-            processes = []
-            affinity = 16
-            i_step = np.concatenate([[1, int(beats.size/2)], np.arange(int(beats.size/2)+int(beats.size/affinity), beats.size, step=int(beats.size/affinity), dtype=np.intp)])
-            i_step[-1] = int(beats.size)
-            for i in range(i_step.size - 1):
-                p = multiprocessing.Process(target=self._loop_finding_routine, args=(beats, i_step[i], i_step[i+1], chroma, power_db, min_duration))
-                processes.append(p)
-                p.daemon=True
-                p.start()
-        else:
-            self._loop_finding_routine(beats, 1, beats.size, chroma, power_db, min_duration)
+        def loop_subroutine(combine_beat_plp=combine_beat_plp, onset_env=onset_env, mel_spectrogram=mel_spectrogram, chroma=chroma, power_db=power_db, beats=beats, avg_dB_diff_threshold=10):
+            if combine_beat_plp:
+                onset_env = librosa.onset.onset_strength(S=mel_spectrogram)
+                pulse = librosa.beat.plp(onset_envelope=onset_env)
+                beats_plp = np.flatnonzero(librosa.util.localmax(pulse))
+                beats = np.union1d(beats, beats_plp)
+                print('Detected {} beats by combining PLP with existing beats'.format(beats.size))
 
-        if concurrency:
-            for process in processes:
-                process.join()
 
-        candidate_pairs = []
+            if concurrency:
+                processes = []
+                affinity = 16
+                i_step = np.concatenate([[1, int(beats.size/2)], np.arange(int(beats.size/2)+int(beats.size/affinity), beats.size, step=int(beats.size/affinity), dtype=np.intp)])
+                i_step[-1] = int(beats.size)
+                for i in range(i_step.size - 1):
+                    p = multiprocessing.Process(target=self._loop_finding_routine, args=(beats, i_step[i], i_step[i+1], chroma, power_db, min_duration, avg_dB_diff_threshold))
+                    processes.append(p)
+                    p.daemon=True
+                    p.start()
+            else:
+                self._loop_finding_routine(beats, 1, beats.size, chroma, power_db, min_duration, avg_dB_diff_threshold)
 
-        while not self._candidate_pairs_q.empty():
-            candidate_pairs.append(self._candidate_pairs_q.get())
+            if concurrency:
+                for process in processes:
+                    process.join()
 
-        print(len(candidate_pairs))
+            candidate_pairs = []
 
-        pruned_list = sorted(candidate_pairs, reverse=False, key=lambda x: x[2])[:keep_at_most]
+            while not self._candidate_pairs_q.empty():
+                candidate_pairs.append(self._candidate_pairs_q.get())
 
-        test_offset = librosa.samples_to_frames( np.amax([int( (bpm / 60) * 0.1 * self.rate ), self.rate * 1.5]) )
-        subseq_beat_sim = [self._subseq_beat_similarity(pruned_list[i][0], pruned_list[i][1], chroma, test_duration=test_offset) for i in range(len(pruned_list))]
+            print(len(candidate_pairs))
 
-        # replace avg_db_diff with cosine similarity
-        for i in range(len(pruned_list)):
-            pruned_list[i] = (pruned_list[i][0], pruned_list[i][1], subseq_beat_sim[i])
+            keep_at_most = np.amax([int(len(candidate_pairs) * 0.25), 10])
 
-        # re-sort based on new score
-        pruned_list = sorted(pruned_list, reverse=True, key=lambda x: x[2])
+            pruned_list = sorted(candidate_pairs, reverse=False, key=lambda x: x[2])[:keep_at_most]
+
+            test_offset = librosa.samples_to_frames( np.amax([int( (bpm / 60) * 0.1 * self.rate ), self.rate * 1.5]) )
+            subseq_beat_sim = [self._subseq_beat_similarity(pruned_list[i][0], pruned_list[i][1], chroma, test_duration=test_offset) for i in range(len(pruned_list))]
+
+            # replace avg_db_diff with cosine similarity
+            for i in range(len(pruned_list)):
+                pruned_list[i] = (pruned_list[i][0], pruned_list[i][1], subseq_beat_sim[i])
+
+            # re-sort based on new score
+            pruned_list = sorted(pruned_list, reverse=True, key=lambda x: x[2])
+            return pruned_list
+
+        pruned_list = loop_subroutine()
+
+        if (len(pruned_list) == 0 or pruned_list[0][2] < 0.90) and combine_beat_plp == False:
+            print('No suitable loop points found with current parameters. Retrying with additional beat points from PLP method.')
+            pruned_list = loop_subroutine(combine_beat_plp=True)
 
         if self.trim_offset[0] > 0:
             offset_f = lambda x: librosa.samples_to_frames(librosa.frames_to_samples(x) + self.trim_offset[0])
@@ -149,6 +158,7 @@ class MusicLooper:
         adjusted_loop_offset = loop_offset * self.channels
 
         i = adjusted_loop_offset - 1000
+        # i = 0
         loop_count = 0
         try:
             while True:
@@ -188,16 +198,9 @@ def loop_track(filename, prioritize_duration=False, start_offset=None, loop_offs
         if start_offset is None and loop_offset is None:
             loop_pair_list = track.find_loop_pairs()
 
-            # TODO: refactor audio preparation code to avoid duplicating work
             if len(loop_pair_list) == 0:
-                print('No loop point found. Retrying with expanded parameters.')
-                loop_pair_list = track.find_loop_pairs(combine_beat_plp=True)
-                if len(loop_pair_list) == 0:
-                    print('No suitable loop point found.')
-                    sys.exit()
-            elif loop_pair_list[0][2] < 0.9:
-                print('Best similarity in strict mode <90%. Retrying with expanded parameters.')
-                loop_pair_list = track.find_loop_pairs(combine_beat_plp=True)
+                print('No suitable loop point found.')
+                sys.exit(1)
 
             if prioritize_duration:
                 loop_pair_list = sorted(loop_pair_list, key=lambda x: np.abs(x[0] - x[1]), reverse=True)
