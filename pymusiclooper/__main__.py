220e769b141cde11b7a44aefba9fae37e646263d
Hazem Nabil <arkrow@protonmail.com>
2020-04-07 23:43:07 +0200

    Restructured source code for pip installation

---
+++ pymusiclooper/__main__.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python3
+# coding=utf-8
+""" PyMusicLooper
+    Copyright (C) 2020  Hazem Nabil
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+
+import argparse
+import os
+import sys
+import time
+
+from .core import MusicLooper
+
+
+def loop_track(filename, loop_start=None, loop_end=None, score=None):
+    try:
+        runtime_start = time.time()
+        # Load the file
+        print("Loading {}...".format(filename))
+
+        track = MusicLooper(filename)
+
+        if loop_start is None and loop_end is None:
+            loop_pair_list = track.find_loop_pairs()
+
+            if len(loop_pair_list) == 0:
+                print("No suitable loop point found.")
+                sys.exit(1)
+
+            loop_start, loop_end, score = loop_pair_list[0]
+
+            track.cache_loop_points(loop_start, loop_end, score)
+
+        runtime_end = time.time()
+        total_runtime = runtime_end - runtime_start
+        print("Total elapsed time (s): {:.3}".format(total_runtime))
+
+        print(
+            "Playing with loop from {} back to {}; similarity: {:.1%})".format(
+                track.frames_to_ftime(loop_end),
+                track.frames_to_ftime(loop_start),
+                score if score is not None else 0,
+            ))
+        print("(press Ctrl+C to exit)")
+
+        track.play_looping(loop_start, loop_end)
+
+    except (TypeError, FileNotFoundError) as e:
+        print("Error: {}".format(e))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="python looper.py",
+        description=
+        "Automatically find loop points in music files and play/export them.",
+    )
+    parser.add_argument("path", type=str, help="path to music file.")
+
+    parser.add_argument(
+        "-p",
+        "--play",
+        action="store_true",
+        default=True,
+        help=
+        "play the song on repeat with the best discovered loop point (default).",
+    )
+    parser.add_argument(
+        "-e",
+        "--export",
+        action="store_true",
+        default=False,
+        help="export the song into intro, loop and outro files (WAV format).",
+    )
+    parser.add_argument(
+        "-j",
+        "--json",
+        action="store_true",
+        default=False,
+        help=
+        "export the loop points (in samples) to a JSON file in the song's directory.",
+    )
+    parser.add_argument(
+        "--disable-cache",
+        action="store_true",
+        default=False,
+        help="skip loading/using cached loop points.",
+    )
+
+    args = parser.parse_args()
+
+    cached_loop_start = None
+    cached_loop_end = None
+    cached_score = None
+
+    dirpath = os.path.dirname(os.path.realpath(__file__))
+    cache_path = os.path.join(dirpath, "cache.json")
+
+    if not args.disable_cache and os.path.exists(cache_path):
+        try:
+            with open(cache_path, "r") as file:
+                cache = json.load(file)
+                full_path = os.path.abspath(args.path)
+                if full_path in cache:
+                    cached_loop_start = cache[full_path]["loop_start"]
+                    cached_loop_end = cache[full_path]["loop_end"]
+                    cached_score = cache[full_path]["score"]
+        except Exception:
+            pass
+
+    if args.export or args.json:
+        track = MusicLooper(args.path)
+
+        if cached_loop_start is not None and cached_loop_end is not None:
+            if args.json:
+                track.export_json(cached_loop_start, cached_loop_end,
+                                  cached_score)
+            if args.export:
+                track.export(cached_loop_start, cached_loop_end)
+            sys.exit(0)
+
+        loop_pair_list = track.find_loop_pairs()
+
+        if len(loop_pair_list) == 0:
+            print("No suitable loop point found.")
+            sys.exit(1)
+
+        loop_start, loop_end, score = loop_pair_list[0]
+
+        track.cache_loop_points(loop_start, loop_end, score)
+
+        if args.json:
+            track.export_json(loop_start, loop_end, score)
+
+        if args.export:
+            track.export(loop_start, loop_end, score)
+
+    elif args.play:
+        loop_track(
+            args.path,
+            loop_start=cached_loop_start,
+            loop_end=cached_loop_end,
+            score=cached_score,
+        )
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def get_requirements():
+    with open('requirements.txt', 'r') as f:
+        requirements = f.read().splitlines()
+    return requirements
+
+
+setuptools.setup(
+    name="pymusiclooper",
+    version="1.0.0",
+    author="arkrow",
+    author_email="arkrow@protonmail.com",
+    description=
+    "Automatically find loop points of any song and play endlessly or export into intro/loop/outro sections or loop points.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arkrow/PyMusicLooper",
+    install_requires=get_requirements(),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Sound/Audio"
+    ],
+    python_requires='>=3.6',
+)
