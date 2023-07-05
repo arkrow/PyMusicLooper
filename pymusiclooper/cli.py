081d238fe890095a8450cf8ed6e5160ee0c5d4cf
Hazem Nabil <arkrow@protonmail.com>
2023-07-05 23:42:54 +0300

    Refactor some console setup to a separate file

--- pymusiclooper/cli.py
+++ pymusiclooper/cli.py
@@ -16,77 +16,15 @@ from click_option_group import RequiredMutuallyExclusiveOptionGroup, optgroup
 from click_params import PUBLIC_URL as UrlParamType
 
 from . import __version__
+from .console import COMMAND_GROUPS, OPTION_GROUPS, rich_console
 from .core import MusicLooper
 from .exceptions import AudioLoadError, LoopNotFoundError
 from .handler import BatchHandler, LoopExportHandler, LoopHandler
 from .youtube import YoutubeDownloader
 
 # CLI --help styling
-_basic_options = ["--path", "--url"]
-_loop_options = [
-    "--min-duration-multiplier",
-    "--min-loop-duration",
-    "--max-loop-duration",
-    "--approx-loop-position",
-    "--brute-force",
-    "--disable-pruning",
-]
-_export_options = ["--output-dir", "--format"]
-_batch_options = ["--recursive", "--flatten"]
-
-
-def _option_groups(additional_basic_options: List[str] = None):
-    if additional_basic_options is not None:
-        combined_basic_options = _basic_options + additional_basic_options
-    else:
-        combined_basic_options = _basic_options
-    return [
-        {
-            "name": "Basic options",
-            "options": combined_basic_options,
-        },
-        {
-            "name": "Advanced loop options",
-            "options": _loop_options,
-        },
-        {
-            "name": "Export options",
-            "options": _export_options,
-        },
-        {
-            "name": "Batch options",
-            "options": _batch_options,
-        },
-    ]
-
-
-_common_option_groups = _option_groups()
-click.rich_click.OPTION_GROUPS = {
-    "pymusiclooper play": _common_option_groups,
-    "pymusiclooper split-audio": _common_option_groups,
-    "pymusiclooper tag": _option_groups(["--tag-names"]),
-    "pymusiclooper export-loop-points": _option_groups(["--export-to"]),
-}
-click.rich_click.COMMAND_GROUPS = {
-    "pymusiclooper": [
-        {
-            "name": "Play Commands",
-            "commands": [
-                "play",
-                "play-tagged"
-            ],
-        },
-        {
-            "name": "Export Commands",
-            "commands": [
-                "export-loop-points",
-                "split-audio",
-                "tag"
-            ],
-        }
-    ]
-}
-
+click.rich_click.OPTION_GROUPS = OPTION_GROUPS
+click.rich_click.COMMAND_GROUPS = COMMAND_GROUPS
 click.rich_click.USE_RICH_MARKUP = True
 # End CLI styling
 
