c1d6bafcf74b0ed98b894399e1a26846d5801f9a
Hazem Nabil <arkrow@protonmail.com>
2023-07-05 14:32:10 +0300

    Added type hints and docstrings to CLI functions

--- pymusiclooper/cli.py
+++ pymusiclooper/cli.py
@@ -3,7 +3,7 @@ import logging
 import os
 import tempfile
 import warnings
-from typing import List
+from typing import List, Optional
 
 import rich_click as click
 from rich.logging import RichHandler
@@ -494,7 +494,16 @@ def tag(
         logging.error(e)
 
 
-def mk_outputdir(path, output_dir):
+def mk_outputdir(path: str, output_dir: Optional[str] = None) -> str:
+    """Creates the output directory in the `path` provided (if it does not exists) and returns the output directory path.
+
+    Args:
+        path (str): The path of the file or directory being processed.
+        output_dir (str, optional): The output directory to use. If None, will create the directory 'LooperOutput' in the path provided.
+
+    Returns:
+        str: The path to the output directory.
+    """
     if os.path.isdir(path):
         default_out = os.path.join(path, "LooperOutput")
     else:
@@ -506,7 +515,16 @@ def mk_outputdir(path, output_dir):
     return output_dir_to_use
 
 
-def download_audio(url, output_dir):
+def download_audio(url: str, output_dir: str) -> str:
+    """Downloads an audio file using yt-dlp from the URL provided and returns its filepath
+
+    Args:
+        url (str): The URL of the stream to pass to yt-dlp to extract audio from
+        output_dir (str): The directory path to store the downloaded audio to
+
+    Returns:
+        str: The filepath of the extracted audio
+    """
     yt = YoutubeDownloader(url, output_dir)
     return yt.filepath
 
